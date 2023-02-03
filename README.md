7dfd93b2394c7ee2464bac52599ec2f6db87e5a1
ilya-zabolotny <34621447+Acex187x@users.noreply.github.com>
2023-02-03 01:16:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# OpenAI Grammy Bot
+Telegram bot written in TypeScript using the Grammy framework to communicate with OpenAI's GPT-3 API.
+
+## Requirements
+- **Node.js** 
+- **ts-node** package (globally installed)
+- **Yarn** or any other Node package manager
+- **OpenAI API key**
+- A Telegram bot token (you can get one from *@BotFather*)
+## Getting started
+1. Clone this repository:
+bash
+
+git clone https://github.com/Acex187x/openai-grammy-bot.git
+2. Install the dependencies:
+```
+yarn install
+```
+3. Create a .env file in the root directory with the following variables:
+```
+OPENAI_API_KEY=<your_openai_api_key>
+TELEGRAM_BOT_TOKEN=<your_telegram_bot_token>
+```
+4. Start the bot:
+```
+yarn start
+```
+## Contributing
+All contributions are welcome. Feel free to open an issue or a pull request.
+
+## License
+This project is licensed under the MIT License.
---
+++ index.ts
@@ -0,0 +1,91 @@
+import { Api, Bot, Context } from "grammy"
+import { Configuration, OpenAIApi } from "openai"
+import {
+	hydrateApi,
+	HydrateApiFlavor,
+	hydrateContext,
+	HydrateFlavor,
+} from "@grammyjs/hydrate"
+import * as dotenv from "dotenv"
+
+dotenv.config()
+
+const configuration = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+})
+const openai = new OpenAIApi(configuration)
+
+const bot = new Bot<HydrateFlavor<Context>, HydrateApiFlavor<Api>>(
+	process.env.BOT_TOKEN
+)
+
+bot.use(hydrateContext())
+bot.api.config.use(hydrateApi())
+
+bot.command("ping", ctx => ctx.reply("Pong"))
+bot.on("message", async ctx => {
+	if (!ctx.message.text) return
+
+	let text = ctx.message.text
+
+	const isAnsweredToMe = ctx.message.reply_to_message?.from?.id === ctx.me.id
+
+	// If message doesn't start with "разум" and it's not private chat and it's not answered to me
+	if (
+		!(
+			text.toLowerCase().startsWith("разум") ||
+			ctx.chat.type === "private" ||
+			isAnsweredToMe
+		)
+	)
+		return
+
+	const reply =
+		ctx.update.message.reply_to_message?.text ||
+		ctx.update.message.reply_to_message?.caption
+
+	if (reply) {
+		text = `Imagine you wrote message "${reply}" and you are asked "${text}", your answer is:`
+	}
+
+	text = text.replace(/разум/gi, " ").trim()
+
+	try {
+		const answer = await ctx.reply("_Работаю над ответом..._", {
+			reply_to_message_id: ctx.message.message_id,
+			parse_mode: "Markdown",
+		})
+
+		openai
+			.createCompletion({
+				model: "text-davinci-003",
+				prompt: text,
+				temperature: 0.2,
+				max_tokens: 800,
+			})
+			.then(async response => {
+				if (!response.data.choices[0].text) {
+					await ctx.api.editMessageText(
+						ctx.chat.id,
+						answer.message_id,
+						"Я не знаю что ответить на это :("
+					)
+				} else {
+					await ctx.api.editMessageText(
+						ctx.chat.id,
+						answer.message_id,
+						response.data.choices[0].text,
+						{
+							parse_mode: "Markdown",
+						}
+					)
+				}
+			})
+	} catch (err) {
+		console.log(err)
+	}
+
+	return
+})
+
+bot.start()
