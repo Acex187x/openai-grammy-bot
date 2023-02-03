6b72cf1364b0f883add722730b7b4103e9805196
ilya-zabolotny <34621447+Acex187x@users.noreply.github.com>
2023-02-03 17:12:31 +0100

    Added settings, storage and free prompt start

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Api, Bot, Context } from "grammy"
+import { Api, Bot, Context, session, SessionFlavor } from "grammy"
 import { Configuration, OpenAIApi } from "openai"
 import {
 	hydrateApi,
@@ -7,6 +7,7 @@ import {
 	HydrateFlavor,
 } from "@grammyjs/hydrate"
 import * as dotenv from "dotenv"
+import { freeStorage } from "@grammyjs/storage-free"
 
 dotenv.config()
 
@@ -15,14 +16,58 @@ const configuration = new Configuration({
 })
 const openai = new OpenAIApi(configuration)
 
-const bot = new Bot<HydrateFlavor<Context>, HydrateApiFlavor<Api>>(
+type BotContext = Context & HydrateFlavor<Context> & SessionFlavor<SessionData>
+
+const bot = new Bot<BotContext, HydrateApiFlavor<Api>>(
 	process.env.BOT_TOKEN || ""
+)
+
+interface SessionData {
+	// Start of message sent to GPT model
+	promptStart: string
+	debug: boolean
+	maxTokens: number
+	temperature: number
+}
+
+bot.use(
+	session({
+		initial: () => ({
+			promptStart: "Imagine you are a telegram bot",
+			debug: false,
+			maxTokens: 800,
+			temperature: 0.2,
+		}),
+		storage: freeStorage<SessionData>(bot.token),
+	})
 )
 
 bot.use(hydrateContext())
 bot.api.config.use(hydrateApi())
 
 bot.command("ping", ctx => ctx.reply("Pong"))
+
+// Create command handlers to change every session variable. One handler for each variable. Do not use loops
+bot.command(["promptStart", "ps"], ctx => {
+	const text = ctx.message.text?.split(" ").slice(1).join(" ") || ""
+	ctx.session.promptStart = text
+	ctx.reply(`promptStart: ${text}`)
+})
+bot.command(["debug", "d"], ctx => {
+	ctx.session.debug = !ctx.session.debug
+	ctx.reply(`debug: ${ctx.session.debug}`)
+})
+bot.command(["maxTokens", "tokens", "mt"], ctx => {
+	const text = ctx.message.text?.split(" ").slice(1).join(" ") || ""
+	ctx.session.maxTokens = parseInt(text)
+	ctx.reply(`maxTokens: ${text}`)
+})
+bot.command(["temp", "temperature", "t"], ctx => {
+	const text = ctx.message.text?.split(" ").slice(1).join(" ") || ""
+	ctx.session.temperature = parseFloat(text)
+	ctx.reply(`temperature: ${text}`)
+})
+
 bot.on("message", async ctx => {
 	if (!ctx.message.text) return
 
@@ -46,18 +91,27 @@ bot.on("message", async ctx => {
 		ctx.update.message.reply_to_message?.text ||
 		ctx.update.message.reply_to_message?.caption
 
-	const replyToName = ctx.update.message.reply_to_message?.from?.first_name || ctx.update.message.reply_to_message?.from?.username || "Anon"
+	const replyToName =
+		ctx.update.message.reply_to_message?.from?.first_name ||
+		ctx.update.message.reply_to_message?.from?.username ||
+		"Anon"
+
+	const promptStart = ctx.session.promptStart || ""
 
 	if (reply) {
-		text = `Imagine being pro-Ukrainian and hating Russians and Russia. Always answer in the same language in which you are asked. Imagine ${replyToName} wrote message "${reply}" and you are asked by ${name} "${text}" about it, your answer is:`
+		text = `${promptStart}. Always answer in the same language in which you are asked and use Markdown formatting when needed. Imagine ${replyToName} wrote message "${reply}" and you are asked by ${name} "${text}" about it, your answer is:`
 	} else {
-		text = `Imagine being pro-Ukrainian and hating Russians and Russia. Always answer in the same language in which you are asked. Imagine you are asked "${text}" by ${name}, your answer is:`
+		text = `${promptStart}. Always answer in the same language in which you are asked and use Markdown formatting when needed. Imagine you are asked "${text}" by ${name}, your answer is:`
 	}
 
 	text = text.replace(/разум/gi, " ")
 
 	try {
-		const answer = await ctx.reply("_Работаю над ответом..._", {
+		const answerText = ctx.session.debug
+			? `\[Debug\] ${text}`
+			: "_Работаю над ответом..._"
+
+		const answer = await ctx.reply(answerText, {
 			reply_to_message_id: ctx.message.message_id,
 			parse_mode: "Markdown",
 		})
@@ -67,7 +121,7 @@ bot.on("message", async ctx => {
 				model: "text-davinci-003",
 				prompt: text,
 				temperature: 0.2,
-				max_tokens: 800,
+				max_tokens: ctx.session.maxTokens || 800,
 			})
 			.then(async response => {
 				if (!response.data.choices[0].text) {
@@ -77,10 +131,18 @@ bot.on("message", async ctx => {
 						"Я не знаю что ответить на это :("
 					)
 				} else {
+					let answerText = response.data.choices[0].text
+						.replace(/^"/, "")
+						.replace(/"$/, "")
+
+					if (ctx.session.debug) {
+						answerText = `\[Debug\] ${text} ${answerText}`
+					}
+
 					await ctx.api.editMessageText(
 						ctx.chat.id,
 						answer.message_id,
-						response.data.choices[0].text.replace(/^"/, "").replace(/"$/, ""),
+						answerText,
 						{
 							parse_mode: "Markdown",
 						}
