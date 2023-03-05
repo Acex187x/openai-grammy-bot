25ede1fb6975fe429e002313a6f3c1c7054431cd
Ilya Zabolotny <34621447+Acex187x@users.noreply.github.com>
2023-03-05 23:37:01 +0100

    QF

--- src/db.ts
+++ src/db.ts
@@ -2,5 +2,5 @@ import { MongoClient } from "mongodb"
 import * as dotenv from "dotenv"
 
 dotenv.config()
-const client = new MongoClient(process.env.MONGO_URL)
+const client = new MongoClient(process.env.MONGO_URL || "")
 export const db = client.db("bot")
