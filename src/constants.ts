c6c3423289bd91a533ff4732ea7f6ac01b646aff
Ilya Zabolotny <34621447+Acex187x@users.noreply.github.com>
2024-01-29 14:16:13 +0100

    Changed BasicPrompt

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,7 @@
-export const BasicPrompt = `For multi-user conversations where users begin messages with [Name]: Message, AI should respond without including the user's name.`
+export const BasicPrompt = ` For multi-user conversations where users begin messages with [Name]: Message, AI should only include it's message and reply  on his own behalf. DO NOT INCLUDE ANY SERVICE MESSAGES OR SYMBOLS IN YOUR REPLY`
 export const SinglePrompt = process.env.SINGLE_PROMPT
 export const IsSinglePrompt = process.env.SINGLE_PROMPT !== undefined
 export const CallSigns = process.env.CALLSIGN?.split(",") || ['разум','mind']
 export const BotName = process.env.BOT_NAME || "Разум"
+
+AI should respond only with text message
